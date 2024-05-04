5987bef90ff95eeab260f41ddfefd990af4539cf
VaibhavSutar <sutarvaibhav588@gmail.com>
2024-05-04 20:00:07 +0530

    "Overriding"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,24 @@
 package com.driver;
 
 class A {
-    String meth() {
+    public String meth() {
         return "Invoking method from class A";
     }
 }
 
 class B extends A {
     @Override
-    String meth() {
+    public String meth() {
         return "Method is overridden in Extended class B";
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        B obj = new B();
+        B objectB = new B();
 
-        System.out.println(obj.meth());
+        System.out.println(objectB.meth());
+
+        System.out.println(((A)objectB).meth());
     }
 }
